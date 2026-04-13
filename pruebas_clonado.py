13ed769baa5da0198100622a90ac0c8f22ec1061
DavidGM2108 <davidalejandro20178@gmail.com>
2026-04-13 15:01:48 -0500

    segundo commit de pruebas de clonado

--- pruebas_clonado.py
+++ pruebas_clonado.py
@@ -2,3 +2,11 @@ print("esto es una prueba de clonado")
 
 for i in range(5):
     print(f"iteración {i}")
+
+a=10
+b=20 
+
+for i in range(a, b):
+    multiplicacion = a * b
+    print(f"la multiplicación de {a} y {b} es: {multiplicacion}")
+    
